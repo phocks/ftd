32eb91fa6b25b74df73ff1c25c047601e681e7ee
Joshua Byrd <phocks@gmail.com>
2022-01-11 14:25:36 +1000

    Return JSON

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-console.log("Listening on http://localhost:8000");
-
-serve((_req) => {
-  return new Response("Hello!", {
-    headers: { "content-type": "text/plain" },
+function handler(_req: Request) {
+  const data = {
+    Hello: "World!",
+  };
+  const body = JSON.stringify(data, null, 2);
+  return new Response(body, {
+    headers: { "content-type": "application/json; charset=utf-8" },
   });
-});
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handler);
